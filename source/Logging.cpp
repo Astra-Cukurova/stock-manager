c1d850f6f47b401a9e926098d8fff8a8ba98908d
doann01 <doganmurt4@gmail.com>
2025-10-25 20:43:13 +0300

    Logging fonksiyonunun çalışan son prototipi

--- source/Logging.cpp
+++ source/Logging.cpp
@@ -8,17 +8,15 @@
 
 using namespace std;
 
-string filename = generateLogFilename();
-
 logger& logger::getInstance() {
     static logger instance;
     return instance;
 }
 
-string generateLogFilename() {
+string logger::generateLogFilename() {
 
 	// Create "logs" folder if it'S not exist
-	std::filesystem::create_directories("logs");
+	filesystem::create_directories("logs");
 
 	// Build a formatted filename
 	auto now = chrono::system_clock::now();
@@ -30,7 +28,7 @@ string generateLogFilename() {
  	return oss.str();
 }
 
-	string getTimeByDay() {
+string logger::getTimeByDay() {
     // Get time string with: hour:minute:second
 	auto now = chrono::system_clock::now();
     time_t t = chrono::system_clock::to_time_t(now);
@@ -40,15 +38,15 @@ string generateLogFilename() {
 	return oss.str();
 }
 
-void logTransaction(logLevel severity, string message) {
+void logger::logTransaction(logLevel severity, string message) {
 	ostringstream oss;
 	oss << "[" << getTimeByDay() << "] "
 		<< "[" << getLevelName(severity) << "] "
 		<< message ;
 	writeToFile(oss.str());
-	}
+}
     	
-void logTransaction(logLevel severity, string firstMessage, int data , string secondMessage) {
+void logger::logTransaction(logLevel severity, string firstMessage, int data , string secondMessage) {
 	ostringstream oss;
 	oss << "[" << getTimeByDay() << "] "
 		<< "[" << getLevelName(severity) << "] "
@@ -58,14 +56,14 @@ void logTransaction(logLevel severity, string firstMessage, int data , string se
 	writeToFile(oss.str());
 }
 
-void writeToFile(const string& message) {
-	ofstream file(filename, ios::app);
+void logger::writeToFile(const string& message) {
+	ofstream file(generateLogFilename(), ios::app);
 	if (file.is_open()) {
 		file << message << "\n";
 	}
 }
 
-string getLevelName(logLevel level) {
+string logger::getLevelName(logLevel level) {
     switch (level) {
         case INFO: return "INFO";
         case WARNING: return "WARNING";
